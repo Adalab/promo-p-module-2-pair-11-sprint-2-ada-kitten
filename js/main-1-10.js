a78158915347204c137d8b5ebae095b7d6aa91fe
miranda-zapata <miranda.zapata@gmail.com>
2022-04-04 10:34:48 +0200

    add lessons 10 and 11 files (for structure)

---
+++ js/main-1-10.js
@@ -0,0 +1,4 @@
+'use strict';
+const listElement = document.querySelector('.js-list');
+
+/* Lección 10 (Métodos funcionales de array). Ejercicio 1. Filtrar por descripción */
---
+++ js/main-1-11.js
@@ -0,0 +1,4 @@
+'use strict';
+const listElement = document.querySelector('.js-list');
+
+/* Lección 11 (Peticiones al servidor). Ejercicio 1. Obtener listado de gatitos desde el servidor */
